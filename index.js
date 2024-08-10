46fb9fd60e81f12bd0b36346f29f3c905721e2c4
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-08-10 13:28:03 -0500

    Unlinked test.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-const chokidar = required('chokidar');
+const chokidar = require('chokidar');
 
 chokidar.watch('.')
     .on('add', () => console.log('FILE ADDED'))
     .on('change', () => console.log('FILE CHANGED'))
     .on('unlink', () => console.log('FILE UNLINKED'));
-    
+
