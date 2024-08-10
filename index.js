5f94fbadcbb85152c76218ca8d58f2e691ff78ad
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-08-10 13:32:27 -0500

    renaming 'add'

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const chokidar = require('chokidar');
 
 chokidar.watch('.')
-    .on('add', () => console.log('FILE ADDED'))
+    .on('add', () => console.log('STARTING USERS PROGRAM'))
     .on('change', () => console.log('FILE CHANGED'))
     .on('unlink', () => console.log('FILE UNLINKED'));
 
