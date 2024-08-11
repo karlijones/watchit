d8d22a3fd51239e2a6b410d74a6a65a8809024c5
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-08-11 11:59:38 -0500

    Updates to change and unlink

--- index.js
+++ index.js
@@ -6,16 +6,19 @@ const program = require('caporal');
 
 program
     .version('0.0.1')
-    .arguement('[filename', 'Name of a file to execute')
+    .argument('[filename', 'Name of a file to execute')
     .action((args) => {
-        console.log(args);
+        const start = debounce(()=> {
+            console.log('STARTING USERS PROGRAM');
+          }, 100);
+          
+        chokidar.watch('.')
+             .on('add', start)
+             .on('change', start)
+             .on('unlink', start);
     });
-const start = debounce(()=> {
-    console.log('STARTING USERS PROGRAM');
-}, 100);
 
-chokidar.watch('.')
-    .on('add', start)
-    .on('change', () => console.log('FILE CHANGED'))
-    .on('unlink', () => console.log('FILE UNLINKED'));
+program.parse(process.argv);
+
+
 
